34e95a6b470bc4cd298af91e414dd0b278e9f260
Mariano Perez <perezm7401@gmail.com>
2022-03-14 17:39:14 -0300

    using bufferedReader

---
+++ client/Client.java
@@ -0,0 +1,44 @@
+package client;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+class Client {
+    Socket client;
+
+    String HOST = "127.0.0.1";
+    int PORT = 8080;
+
+    DataOutputStream out;
+    
+    Client() throws IOException {
+        client = new Socket(HOST, PORT);
+        out = new DataOutputStream(client.getOutputStream());
+    }
+
+    public void start() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese \"0\" para salir, o cualq cosa para" + 
+        " registrar un cliente");
+
+        while (!scanner.nextLine().equalsIgnoreCase("0")) {
+            out.writeUTF("cliente\n");
+        }
+        scanner.close();
+        client.close();
+        System.out.println("Client closed");
+    }               
+    
+    public static void main(String[] args) {
+        try {
+            Client cl = new Client();
+            cl.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/Server.java
@@ -0,0 +1,62 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+    ServerSocket server;
+    Socket client;
+    int PORT = 8080;
+    BufferedReader in;
+    DataOutputStream out;
+
+    int clientCount;
+
+    void addClient() {clientCount++;}
+    int getClientCount() {return this.clientCount;}
+
+    Server() throws IOException {
+        server = new ServerSocket(PORT);
+    }
+    
+    public void start() throws IOException {
+        client = server.accept();
+        System.out.println("Server online");
+
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        
+        String msg;
+        // boolean shutdown = false;
+        // while (!shutdown) {
+        //     if (in.ready()) {
+                while ((msg = in.readLine()) != null) {
+                    System.out.println("reading...");
+
+                    addClient();
+                    System.out.println(msg);
+                    System.out.println("Client count: " + clientCount);
+                }
+                // shutdown = true;
+        //     }
+        // }
+
+        System.out.println("server offline");
+        client.close();
+        server.close();       
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("program start");
+            Server sv = new Server();
+            sv.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
